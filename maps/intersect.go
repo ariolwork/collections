3576fd0455ab02e678be8f5efa29866efbf74410
Artem Danko <artemdanko@Artems-MacBook-Pro-2.local>
2026-03-05 01:23:57 +0300

    add all comments

--- maps/intersect.go
+++ maps/intersect.go
@@ -1,17 +1,24 @@
 package maps
 
+// The Append function append values from second map to first.
+// Source map changing
 func Append[M ~map[K]V, K comparable, V any](first, second M) {
 	for key, val := range second {
 		first[key] = val
 	}
 }
 
+// The Join function join values from all incoming maps to one.
+// Source maps r not changing
+// Equals keys override value in result map. If all values must bee
+// saved, BucketingJoin should be used
+// Rigth way to use
+//
+//	result := maps.Join(map[int]int{1:1}, map[int]int{2:1}, map[int]int{3:1})
 func Join[M ~map[K]V, K comparable, V any](maps ...M) map[K]V {
 	initSize := 0
 	for _, m := range maps {
-		if initSize < len(m) {
-			initSize = len(m)
-		}
+		initSize += len(m)
 	}
 
 	result := make(map[K]V, initSize)
@@ -24,6 +31,13 @@ func Join[M ~map[K]V, K comparable, V any](maps ...M) map[K]V {
 
 	return result
 }
+
+// The BucketingJoin function join values from all incoming maps to one.
+// Source maps r not changing
+// Equals keys put values to slice
+// Rigth way to use
+//
+//	result := maps.BucketingJoin(map[int]int{1:1}, map[int]int{1:3, 2:1}, map[int]int{1:4, 3:1})
 
 func BucketingJoin[M ~map[K]V, K comparable, V any](maps ...M) map[K][]V {
 	initSize := 0
@@ -44,6 +58,8 @@ func BucketingJoin[M ~map[K]V, K comparable, V any](maps ...M) map[K][]V {
 	return result
 }
 
+// The IntersectKeys function intersect keys of maps.
+// Function returns keys, contaning in all of maps
 func IntersectKeys[M ~map[K]V, K comparable, V any](maps ...M) []K {
 	initSize := 0
 	for _, m := range maps {
--- maps/transform.go
+++ maps/transform.go
@@ -1,5 +1,12 @@
 package maps
 
+// The UpdateValues function convert each element value of map.
+// It creates new map with updated vavlues. If current map
+// values updating required, use UpdateValues
+// Right way to use:
+//
+//	map := map[int]int{1:1, 2:2}
+//	mapOfStrings := maps.TransformValues(map, func(k, v int) string {return itoa.Itoa(v)})
 func TransformValues[M ~map[K]V, K comparable, V, R any](m M, applyFunc func(K, V) R) map[K]R {
 	result := make(map[K]R, len(m))
 
@@ -10,6 +17,14 @@ func TransformValues[M ~map[K]V, K comparable, V, R any](m M, applyFunc func(K, 
 	return result
 }
 
+// The TransformKeys function convert each element key of map.
+// It creates new map with updated vavlues.
+// !!!If new keys have similar values, values will be overriten.
+// To save all values, use Rotate function
+// Right way to use:
+//
+//	map := map[int]int{1:1, 2:2}
+//	stringKeysMap := maps.TransformKeys(map, func(k, v int) string {return itoa.Itoa(k)})
 func TransformKeys[M ~map[K]V, K, R comparable, V any](m M, applyFunc func(K, V) R) map[R]V {
 	result := make(map[R]V, len(m))
 
@@ -20,6 +35,13 @@ func TransformKeys[M ~map[K]V, K, R comparable, V any](m M, applyFunc func(K, V)
 	return result
 }
 
+// The Rotate function select new keys for each element of map.
+// It creates new rotated map.
+// !!!If new keys have similar values, values will be saved in slice
+// Right way to use:
+//
+//	map := map[int]int{1:1, 2:2}
+//	stringKeysMap := maps.Rotate(map, func(k, v int) string {return itoa.Itoa(k)})
 func Rotate[M ~map[K]V, K, R comparable, V any](m M, applyFunc func(K, V) R) map[R][]V {
 	result := make(map[R][]V, len(m))
 
